e4d5cf04cb380f3689b16197e3e1fb46d36cc0a6
david <david@xantus.network>
2024-04-15 15:48:45 +0700

    feat: init

---
+++ components/partial/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="container">
+    <NuxtLink to="/">
+      <h2>Logo</h2>
+    </NuxtLink>
+    <ul>
+      <li>
+        <NuxtLink to="/">Page 1</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">Page 2</NuxtLink>
+      </li>
+    </ul>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative overflow-hidden">
+    <PartialHeader />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
