666da18d4170cfd35f235230167fbf3272013894
david <david@xantus.network>
2024-04-15 16:08:01 +0700

    feat: add tailwind

--- components/partial/Header.vue
+++ components/partial/Header.vue
@@ -1,15 +1,17 @@
 <template>
-  <header class="container">
-    <NuxtLink to="/">
-      <h2>Logo</h2>
-    </NuxtLink>
-    <ul>
-      <li>
-        <NuxtLink to="/">Page 1</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/">Page 2</NuxtLink>
-      </li>
-    </ul>
+  <header class="h-16 flex items-center">
+    <div class="container flex items-center">
+      <NuxtLink to="/">
+        <h2>Logo</h2>
+      </NuxtLink>
+      <ul class="flex-1 flex items-center justify-end gap-6">
+        <li>
+          <NuxtLink to="/">Page 1</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/">Page 2</NuxtLink>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,6 @@
     <main>
       <slot />
     </main>
+    <!-- <PartialFooter /> -->
   </div>
 </template>
