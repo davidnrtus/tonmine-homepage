1df6dabf4e8dc4e67ce73af5297d753cf1dde96a
david <david@xantus.network>
2024-05-16 14:26:07 +0700

    feat: add shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+const animate = require('tailwindcss-animate')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        lg: '2rem',
+      },
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        system: {
+          black: '#0B0B0F',
+        },
+        visual: {
+          yellow: {
+            500: '#FADB14',
+          },
+        },
+        neutral: {
+          900: '#131313',
+          300: '#B1B1B1',
+          200: '#CACACA',
+        },
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [animate],
+}
